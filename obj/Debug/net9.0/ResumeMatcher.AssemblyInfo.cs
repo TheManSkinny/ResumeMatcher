46bb9bd8fd58080ea9669bfeea82f69f8db099a3
TheManSkinny <amponsahmichael664@gmail.com>
2025-06-28 21:13:07 +0000

    initial commit

--- obj/Debug/net9.0/ResumeMatcher.AssemblyInfo.cs
+++ obj/Debug/net9.0/ResumeMatcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeMatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ed8b119b48e1bf7ba43fc800883bb5fb6f298a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a51ab002c5ff98cbbff1c131ba77ec69833684")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeMatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeMatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
